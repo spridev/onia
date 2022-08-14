dba6036798329c94b075e9529a617bb512483bde
Joé Donzallaz <spri.development@gmail.com>
2022-08-14 18:19:25 +0200

    feat(drum): clean accepts a single path

--- packages/drum/src/plugins/clean.ts
+++ packages/drum/src/plugins/clean.ts
@@ -7,17 +7,19 @@ import type { Plugin } from 'rollup';
 /**
  * Clean the output directory.
  */
-export function clean(paths: string[]): Plugin {
+export function clean(paths: string | string[]): Plugin {
   return {
     name: 'clean',
     async buildStart(): Promise<void> {
+      paths = Array.isArray(paths) ? paths : [paths];
+
       for (const path of paths) {
         if (Fs.existsSync(path)) {
           Fs.rmSync(path, { recursive: true });
         }
       }
 
-      console.log(red(`deleted ${bold(paths.join(', '))}`));
+      console.log(red(`deleted ${bold(paths.join(', '))}...`));
     },
   };
 }
