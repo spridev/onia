3f5df38b50c24c707445e1a690f04169eeb1051d
Joé Donzallaz <spri.development@gmail.com>
2022-08-29 09:02:05 +0200

    test(duck): add unit tests

---
+++ packages/duck/tests/atomic-read.test.ts
@@ -0,0 +1,126 @@
+import anyTest, { TestFn } from 'ava';
+
+import {
+  DynamoDBClient,
+  TransactGetItemsCommand,
+} from '@aws-sdk/client-dynamodb';
+import { marshall } from '@aws-sdk/util-dynamodb';
+
+import { ClientMock, ClientType } from '@onia/mock';
+
+import { AtomicRead } from '../src';
+
+interface Item {
+  PK: string;
+}
+
+interface TestContext {
+  mock: ClientType<DynamoDBClient>;
+}
+
+const ddb = new DynamoDBClient({});
+
+const test = anyTest as TestFn<TestContext>;
+
+test.beforeEach(function (t) {
+  t.context.mock = new ClientMock(DynamoDBClient);
+});
+
+test.afterEach.always(function (t) {
+  t.context.mock.restore();
+});
+
+test('returns multiple items', async function (t) {
+  const { mock } = t.context;
+
+  mock.on(TransactGetItemsCommand).resolves({
+    Responses: [
+      { Item: { PK: { S: '1' } } },
+      { Item: { PK: { S: '2' } } },
+      { Item: undefined },
+    ],
+  });
+
+  const read = new AtomicRead(ddb)
+    .get({ TableName: 'onia', Key: marshall({ PK: '1' }) })
+    .get({ TableName: 'onia', Key: marshall({ PK: '2' }) })
+    .get({ TableName: 'duck', Key: marshall({ PK: '3' }) });
+
+  const items = await read.commit<Item>();
+
+  t.deepEqual(items, [{ PK: '1' }, { PK: '2' }, undefined]);
+
+  t.deepEqual(mock.call(0), {
+    TransactItems: [
+      {
+        Get: {
+          TableName: 'onia',
+          Key: { PK: { S: '1' } },
+        },
+      },
+      {
+        Get: {
+          TableName: 'onia',
+          Key: { PK: { S: '2' } },
+        },
+      },
+      {
+        Get: {
+          TableName: 'duck',
+          Key: { PK: { S: '3' } },
+        },
+      },
+    ],
+  });
+});
+
+test('returns an empty array when the inputs are empty', async function (t) {
+  const { mock } = t.context;
+
+  mock.on(TransactGetItemsCommand).resolves({});
+
+  const read = new AtomicRead(ddb);
+
+  const items = await read.commit<Item>();
+
+  t.deepEqual(items, []);
+
+  t.is(mock.count(), 0);
+});
+
+test('returns an empty array when the responses are empty', async function (t) {
+  const { mock } = t.context;
+
+  mock.on(TransactGetItemsCommand).resolves({
+    Responses: undefined,
+  });
+
+  const read = new AtomicRead(ddb)
+    .get({ TableName: 'onia', Key: marshall({ PK: '1' }) })
+    .get({ TableName: 'onia', Key: marshall({ PK: '2' }) })
+    .get({ TableName: 'duck', Key: marshall({ PK: '3' }) });
+
+  const items = await read.commit<Item>();
+
+  t.deepEqual(items, []);
+
+  t.is(mock.count(), 1);
+});
+
+test('throws when the collection size limit is exceeded', async function (t) {
+  const { mock } = t.context;
+
+  mock.on(TransactGetItemsCommand).resolves({});
+
+  const read = new AtomicRead(ddb);
+
+  for (let index = 0; index < 26; index++) {
+    read.get({ TableName: 'onia', Key: marshall({ PK: String(index) }) });
+  }
+
+  await t.throwsAsync(() => read.commit<Item>(), {
+    message: 'Collection size limit exceeded',
+  });
+
+  t.is(mock.count(), 0);
+});
---
+++ packages/duck/tests/atomic-write.test.ts
@@ -0,0 +1,199 @@
+import anyTest, { TestFn } from 'ava';
+
+import {
+  DynamoDBClient,
+  TransactionCanceledException,
+  TransactWriteItemsCommand,
+  TransactWriteItemsCommandInput,
+} from '@aws-sdk/client-dynamodb';
+import { marshall } from '@aws-sdk/util-dynamodb';
+
+import { ConditionExpression, UpdateExpression } from '@onia/dino';
+import { ClientMock, ClientType } from '@onia/mock';
+
+import { AtomicWrite, hash } from '../src';
+
+interface TestContext {
+  mock: ClientType<DynamoDBClient>;
+}
+
+const ddb = new DynamoDBClient({});
+
+const test = anyTest as TestFn<TestContext>;
+
+test.beforeEach(function (t) {
+  t.context.mock = new ClientMock(DynamoDBClient);
+});
+
+test.afterEach.always(function (t) {
+  t.context.mock.restore();
+});
+
+test('writes multiple items', async function (t) {
+  const { mock } = t.context;
+
+  mock.on(TransactWriteItemsCommand).resolves({});
+
+  const write = new AtomicWrite(ddb)
+    .put({
+      TableName: 'onia',
+      Item: marshall({ PK: '1' }),
+    })
+    .update({
+      TableName: 'onia',
+      Key: marshall({ PK: '2' }),
+      UpdateExpression: new UpdateExpression().set('name', 'duck'),
+    })
+    .delete({
+      TableName: 'duck',
+      Key: marshall({ PK: '3' }),
+    })
+    .condition({
+      TableName: 'duck',
+      Key: marshall({ PK: '4' }),
+      ConditionExpression: new ConditionExpression().exists('PK'),
+    });
+
+  await write.commit();
+
+  t.deepEqual(mock.call(0), {
+    TransactItems: [
+      {
+        Put: {
+          TableName: 'onia',
+          Item: { PK: { S: '1' } },
+        },
+      },
+      {
+        Update: {
+          TableName: 'onia',
+          Key: { PK: { S: '2' } },
+          UpdateExpression: 'SET #name0 = :value1',
+          ExpressionAttributeNames: { '#name0': 'name' },
+          ExpressionAttributeValues: { ':value1': { S: 'duck' } },
+        },
+      },
+      {
+        Delete: {
+          TableName: 'duck',
+          Key: { PK: { S: '3' } },
+        },
+      },
+      {
+        ConditionCheck: {
+          TableName: 'duck',
+          Key: { PK: { S: '4' } },
+          ConditionExpression: 'attribute_exists(#name0)',
+          ExpressionAttributeNames: { '#name0': 'PK' },
+        },
+      },
+    ],
+  });
+});
+
+test('sets the transaction token', async function (t) {
+  const { mock } = t.context;
+
+  mock.on(TransactWriteItemsCommand).resolves({});
+
+  const write = new AtomicWrite(ddb)
+    .put({ TableName: 'onia', Item: marshall({ PK: '1' }) })
+    .put({ TableName: 'onia', Item: marshall({ PK: '2' }) });
+
+  const tokens = [
+    'a',
+    'a-short-token',
+    'a-very-long-and-boring-token-which-exceeds-36-characters',
+  ];
+
+  for (const token of tokens) {
+    await write.token(token).commit();
+
+    const input = mock.last() as TransactWriteItemsCommandInput;
+
+    t.is(input.ClientRequestToken, hash(token));
+
+    t.assert(input.ClientRequestToken.length >= 5);
+    t.assert(input.ClientRequestToken.length <= 36);
+  }
+});
+
+test('returns early when the inputs are empty', async function (t) {
+  const { mock } = t.context;
+
+  mock.on(TransactWriteItemsCommand).resolves({});
+
+  const write = new AtomicWrite(ddb);
+
+  await write.commit();
+
+  t.is(mock.count(), 0);
+});
+
+test('throws when the collection size limit is exceeded', async function (t) {
+  const { mock } = t.context;
+
+  mock.on(TransactWriteItemsCommand).resolves({});
+
+  const write = new AtomicWrite(ddb);
+
+  for (let index = 0; index < 26; index++) {
+    write.put({ TableName: 'onia', Item: marshall({ PK: String(index) }) });
+  }
+
+  await t.throwsAsync(() => write.commit(), {
+    message: 'Collection size limit exceeded',
+  });
+
+  t.is(mock.count(), 0);
+});
+
+test('handles transaction cancellation', async function (t) {
+  const { mock } = t.context;
+
+  const error = new TransactionCanceledException({
+    $metadata: {},
+    CancellationReasons: [{ Code: undefined }, { Code: 'duck' }],
+  });
+
+  mock.on(TransactWriteItemsCommand).rejects(error);
+
+  const write = new AtomicWrite(ddb)
+    .put(
+      { TableName: 'onia', Item: marshall({ PK: '1' }) },
+      {
+        onError() {
+          t.fail();
+        },
+      }
+    )
+    .put(
+      { TableName: 'onia', Item: marshall({ PK: '2' }) },
+      {
+        onError(reason) {
+          t.is(reason.Code, 'duck');
+        },
+      }
+    );
+
+  await t.throwsAsync(() => write.commit(), { is: error });
+
+  t.plan(2);
+});
+
+test('ignores transaction cancellation when the reasons are empty', async function (t) {
+  const { mock } = t.context;
+
+  const error = new TransactionCanceledException({
+    $metadata: {},
+    CancellationReasons: undefined,
+  });
+
+  mock.on(TransactWriteItemsCommand).rejects(error);
+
+  const write = new AtomicWrite(ddb)
+    .put({ TableName: 'onia', Item: marshall({ PK: '1' }) })
+    .put({ TableName: 'onia', Item: marshall({ PK: '2' }) });
+
+  await t.throwsAsync(() => write.commit(), { is: error });
+});
